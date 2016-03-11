fba558bba7ac5ccac9c2a9e169561936819d7480
Jonathan Damico <jj_damico@yahoo.com>
2016-03-11 12:41:08 -0800

    fix unsecured matches

--- js.js
+++ js.js
@@ -209,10 +209,10 @@ $(document).ready(function() {
       });
       currentMatchNumber = 0;
       differience = 0;
-      for (i = 0; i < jd.length; i++) {
-        if (jd[i].alliances.red.score == -1) {
-          currentMatchNumber = jd[i].match_number;
-          $('#currentmatch').append('Current Match Number: ' + jd[i].match_number);
+      for (i = jd.length-1; i >= 0; i++) {
+        if (jd[i].alliances.red.score != -1) {
+          currentMatchNumber = jd[i+1].match_number;
+          $('#currentmatch').append('Current Match Number: ' + jd[i+1].match_number);
           break;
         }
       }
